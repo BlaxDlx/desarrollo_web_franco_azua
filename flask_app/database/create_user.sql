6d4b0bb80fca121bb34ea8c11782e2b7f30a1fe1
BlaxDlx <franco.azua@ug.uchile.cl>
2025-05-16 01:14:35 -0400

    ✨ feat: Se agregaron modelos y funciones CRUD para actividad, comuna, región, foto, contactar_por y actividad_tema

---
+++ flask_app/database/create_user.sql
@@ -0,0 +1,7 @@
+-- Active: 1747351083692@@127.0.0.1@3306@mysql
+-- Crear Usuario
+
+CREATE USER 'cc5002'@'localhost' IDENTIFIED BY 'programacionweb';
+
+-- Eliminar usuario de ser necesario
+DROP USER 'cc5002'@'localhost';
---
+++ flask_app/database/db.py
@@ -0,0 +1,10 @@
+import pymysql
+import json
+
+DB_NAME = 'tarea2'
+DB_USERNAME = 'cc5002'
+DB_PASSWORD = 'programacionweb'
+DB_HOST = 'localhost'
+DB_PORT = 3306
+DB_CHARSET = 'utf8'
+
