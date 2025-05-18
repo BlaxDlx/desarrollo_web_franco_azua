830086a96f48e325f00c81ef9e51d99dbeecd97b
BlaxDlx <franco.azua@ug.uchile.cl>
2025-05-17 22:27:41 -0400

    feat: Se añaden las actividades de ejemplo a la data base y archivos para iniciar la database y dropearlas

--- flask_app/database/create_user.sql
+++ flask_app/database/create_user.sql
@@ -1,4 +1,4 @@
--- Active: 1747351083692@@127.0.0.1@3306@mysql
+-- Active: 1747361000098@@127.0.0.1@3306@tarea2
 -- Crear Usuario
 
 CREATE USER 'cc5002'@'localhost' IDENTIFIED BY 'programacionweb';
