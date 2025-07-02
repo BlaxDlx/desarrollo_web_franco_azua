a757bbba6ee35d98f8f18a2e3e267952449270a4
BlaxDlx <franco.azua@ug.uchile.cl>
2025-07-02 02:36:14 -0400

    refactor: Pequeño cambio de archivos internos en la carpeta flask_app

--- flask_app/venv/Scripts/activate.bat
+++ flask_app/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\BlaxDlx\Documents\UNIVERSIDAD\DCC\5to Semestre\Apps web\Tarea 1\flask_app\venv
+set VIRTUAL_ENV=C:\Users\BlaxDlx\Documents\UNIVERSIDAD\DCC\5to Semestre\Apps web\Proyecto\flask_app\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
